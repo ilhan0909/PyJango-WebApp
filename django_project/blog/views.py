eed42628efeca4e929eb91f6e5405a36928ac9cd
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-05-04 16:47:00 +0300

    added dummy data and changed to render from httpresponse for convenience

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+postlist = [
+    {
+        'author': 'İlhan',
+        'title': 'BP1',
+        'content': 'FP CONTENT',
+        'date_posted': '27.04.2022'
+    },
+    {
+        'author': 'Abdullah',
+        'title': 'BP2',
+        'content': 'SP CONTENT',
+        'date_posted': '29.04.2022'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': postlist,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
